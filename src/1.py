e3b1fc698e6a1db9cad96592baeb22b40ddde1fa
Ruo Ando <ando.ruo@gmail.com>
2017-03-08 15:06:42 +0900

    line reader

---
+++ src/1.py
@@ -0,0 +1,14 @@
+# python 1.py tools/libxl/libxl.c libxl 372
+
+import sys 
+
+argvs = sys.argv  
+argc = len(argvs) 
+
+f = open(argvs[1])
+
+line = f.readline() 
+
+while line:
+    print line
+    line = f.readline() 
